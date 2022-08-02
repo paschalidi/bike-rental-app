14c8090d26fe3cfd1ca10bb8d01cb700ec390600
Christos Paschalidis <paschalidi@outlook.com>
2022-08-02 20:47:04 +0200

    adds new page under /manager/bikes

---
+++ pages/manager/bikes.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import { Box, Grid, Page, PageContent } from 'grommet';
+import { TopBar } from '../../components/TopBar';
+import { SideNav } from '../../components/SideNav';
+import { BikeTable } from '../../components/BikeTable';
+
+const ManagerDashboard: NextPage = () => (
+  <Grid
+    fill
+    rows={['auto', 'flex']}
+    columns={['auto', 'flex']}
+    areas={[
+      { name: 'header', start: [0, 0], end: [1, 0] },
+      { name: 'sidebar', start: [0, 1], end: [0, 1] },
+      { name: 'main', start: [1, 1], end: [1, 1] },
+    ]}
+  >
+    <Box gridArea="header" background="brand">
+      <TopBar />
+    </Box>
+    <Box gridArea="sidebar" background="light-5">
+      <SideNav />
+    </Box>
+    <Box gridArea="main">
+      <Page kind="narrow">
+        <PageContent>
+          <BikeTable />
+        </PageContent>
+      </Page>
+    </Box>
+  </Grid>
+);
+
+export default ManagerDashboard;
