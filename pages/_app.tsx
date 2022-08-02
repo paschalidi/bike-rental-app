63880786dcff298ca7b7d2fe6669d495e44a74df
Christos Paschalidis <paschalidi@outlook.com>
2022-08-02 15:54:48 +0200

    installs firebase and grommet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { Grommet } from 'grommet';
+import { AuthContextProvider } from '../contexts/auth';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Grommet className="App">
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  </Grommet>
+);
 
 export default MyApp;
