88f4a06de08a0bc05ff0d7f0adbc9299aa9661c5
Christos Paschalidis <paschalidi@outlook.com>
2022-08-03 16:48:32 +0200

    implements homepage depending on the role

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,76 @@
 import type { NextPage } from 'next';
-import { Heading, Page, PageContent } from 'grommet';
+import { Button, Heading, Page, PageContent } from 'grommet';
 import React from 'react';
+import { useRouter } from 'next/router';
+import { Roles, useAuth } from '../src/contexts/auth';
+
 import { TopBar } from '../src/components/TopBar';
 
-const Home: NextPage = () => (
-  <>
-    <TopBar />
-    <Page kind="narrow">
-      <PageContent>
-        <Heading> Hello from home</Heading>
-      </PageContent>
-    </Page>
-  </>
-);
+const Home: NextPage = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  if (user?.role === Roles.User) {
+    return (
+      <>
+        <TopBar />
+        <Page kind="narrow">
+          <PageContent>
+            <Heading>Want to rent some bikes?</Heading>
+            <Button
+              primary
+              label="GO"
+              size="large"
+              onClick={() => {
+                router.push(`/${user?.role}/bikes`);
+              }}
+            />
+          </PageContent>
+        </Page>
+      </>
+    );
+  }
+  if (user?.role === Roles.Manager) {
+    return (
+      <>
+        <TopBar />
+        <Page kind="narrow">
+          <PageContent>
+            <Heading>Want to view & edit some bikes?</Heading>
+            <Button
+              primary
+              label="GO"
+              size="large"
+              onClick={() => {
+                router.push(`/${user?.role}/bikes`);
+              }}
+            />
+          </PageContent>
+        </Page>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <TopBar />
+      <Page kind="narrow">
+        <PageContent>
+          <Heading style={{marginTop:'10vh'}}>
+            To rent bikes please sign up first
+          </Heading>
+          <Button
+            primary
+            label="SING UP NOW"
+            size="large"
+            onClick={() => {
+              router.push(`/signup`);
+            }}
+          />
+        </PageContent>
+      </Page>
+    </>
+  );
+};
 
 export default Home;
