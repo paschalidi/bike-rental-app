c2c46a77d65cf7c03f6330df2d4a0289bd3c82cc
Christos Paschalidis <paschalidi@outlook.com>
2022-08-04 09:23:22 +0200

    rating is not a number but a string

--- src/components/AddNewBikeForm/index.tsx
+++ src/components/AddNewBikeForm/index.tsx
@@ -24,7 +24,7 @@ export const AddNewBikeForm = () => {
       color: '',
       location: '',
       available: false,
-      rating: 5,
+      rating: "5",
     },
     validate: formValidation,
     onSubmit: async (
